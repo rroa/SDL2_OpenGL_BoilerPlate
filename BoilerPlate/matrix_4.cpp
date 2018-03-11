3bd744957fe23af28ba6b9f4bb61adfe496b91ad
java7f <java7f@gmail.com>
2018-03-11 18:47:27 -0400

    some matrix_4 implementations

---
+++ BoilerPlate/matrix_4.cpp
@@ -0,0 +1,114 @@
+#include "matrix_4.hpp"
+
+
+engine::matrix_4::matrix_4()
+{
+	set_identity();
+	mAngle = Vector3();
+}
+
+engine::matrix_4::matrix_4(float value1, float value2, float value3, float value4, float value5, float value6, float value7, float value8, float value9, float value10, float value11, float value12, float value13, float value14, float value15, float value16)
+{
+	mMatrix[0][0] = value1;
+	mMatrix[1][0] = value2;
+	mMatrix[2][0] = value3;
+	mMatrix[3][0] = value4;
+
+	mMatrix[0][1] = value5;
+	mMatrix[1][1] = value6;
+	mMatrix[2][1] = value7;
+	mMatrix[3][1] = value8; 
+
+	mMatrix[0][2] = value9;
+	mMatrix[1][2] = value10;
+	mMatrix[2][2] = value11;
+	mMatrix[3][2] = value12;
+
+	mMatrix[0][3] = value13;
+	mMatrix[1][3] = value14;
+	mMatrix[2][3] = value15;
+	mMatrix[3][3] = value16;
+}
+
+engine::matrix_4::matrix_4(float values[])
+{
+
+}
+
+
+engine::matrix_4::~matrix_4()
+{
+}
+
+void engine::matrix_4::set_identity()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (i == j)
+			{
+				mMatrix[j][i] = 1;
+			}
+			else
+			{
+				mMatrix[j][i] = 0;
+			}
+		}
+	}
+}
+
+Vector3 engine::matrix_4::get_angle()
+{
+	return mAngle;
+}
+
+float *engine::matrix_4::get_matrix()
+{
+	return *mMatrix;
+}
+
+engine::matrix_4 engine::matrix_4::get_transpose()
+{
+	engine::matrix_4 transposedMatrix;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			transposedMatrix.mMatrix[j][i] = mMatrix[i][j];
+		}
+	}
+
+	return transposedMatrix;
+}
+
+engine::matrix_4 engine::matrix_4::operator+(const matrix_4 &pToAdd) const
+{
+	matrix_4 newMatrix;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			newMatrix.mMatrix[j][i] = mMatrix[j][i] + pToAdd.mMatrix[j][i];
+		}
+	}
+
+	return newMatrix;
+}
+
+engine::matrix_4 engine::matrix_4::operator-(const matrix_4 &pToSubstract) const
+{
+	matrix_4 newMatrix;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			newMatrix.mMatrix[j][i] = mMatrix[j][i] - pToSubstract.mMatrix[j][i];
+		}
+	}
+
+	return newMatrix;
+}
---
+++ BoilerPlate/matrix_4.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "Vector3.hpp"
+
+namespace engine {
+
+	class matrix_4
+	{
+	public:
+		matrix_4();
+		matrix_4(float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float);
+		matrix_4(float[]);
+		~matrix_4();
+
+		float *get_matrix();
+		matrix_4 get_transpose();
+		void set_identity();
+		Vector3 get_angle();
+
+		matrix_4 operator + (const matrix_4&) const;
+		matrix_4 operator - (const matrix_4&) const;
+		matrix_4 operator * (const matrix_4&) const;
+		matrix_4 operator / (const matrix_4&) const;
+
+	private:
+		float mMatrix[4][4];
+		Vector3 mAngle;
+	};
+}
