5ad24233e53f3736f643d46b4af589333df7203b
Arlet Castillo <arletmarcel24@gmail.com>
2018-03-12 16:21:55 -0400

    Added translate and rotate functions

--- BoilerPlate/matrix_4.hpp
+++ BoilerPlate/matrix_4.hpp
@@ -1,6 +1,12 @@
 #pragma once
-#include "Vector_3.hpp"
+
+#ifndef MATRIX_4_HPP
+#define MATRIX_4_HPP
+
 #include <iostream>
+
+#include "Vector_3.hpp"
+#include "Vector_4.hpp"
 
 namespace Engine {
 
@@ -23,6 +29,10 @@ namespace Engine {
 		matrix_4 get_transpose();
 		void set_identity();
 		Vector_3 get_angle();
+		matrix_4 translate(Vector_4);
+		matrix_4 rotate_x(float);
+		matrix_4 rotate_y(float);
+		matrix_4 rotate_z(float);
 
 		float& operator [] (const int);
 		friend std::ostream& operator<<(std::ostream&, const matrix_4&);
@@ -37,3 +47,5 @@ namespace Engine {
 		array_2D mMatrix;
 	};
 }
+
+#endif // _MATRIX_4_H_
