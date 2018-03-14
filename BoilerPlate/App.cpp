ecfe193cd46c40d6aab8100a59a8cc4b1a2ffb9a
java7f <java7f@gmail.com>
2018-03-14 13:04:53 -0400

    Changing the Boiler Plate to OpenGL pipeline

--- BoilerPlate/App.cpp
+++ BoilerPlate/App.cpp
@@ -6,10 +6,116 @@
 #include <GL/glew.h>
 #include <SDL2/SDL_opengl.h>
 
+//Move this
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
 namespace Engine
 {
 	const float DESIRED_FRAME_RATE = 60.0f;
 	const float DESIRED_FRAME_TIME = 1.0f / DESIRED_FRAME_RATE;
+
+	//TO DO: Abstract this
+	GLuint VertexArrayObject; //VAO
+	GLuint VertexBufferObject; //VBO
+	GLuint ProgramID;
+
+	GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path) {
+
+		// Create the shaders
+		GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+		GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+		// Read the Vertex Shader code from the file
+		std::string VertexShaderCode;
+		std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+		if (VertexShaderStream.is_open()) {
+			std::stringstream sstr;
+			sstr << VertexShaderStream.rdbuf();
+			VertexShaderCode = sstr.str();
+			VertexShaderStream.close();
+		}
+		else {
+			printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
+			getchar();
+			return 0;
+		}
+
+		// Read the Fragment Shader code from the file
+		std::string FragmentShaderCode;
+		std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+		if (FragmentShaderStream.is_open()) {
+			std::stringstream sstr;
+			sstr << FragmentShaderStream.rdbuf();
+			FragmentShaderCode = sstr.str();
+			FragmentShaderStream.close();
+		}
+
+		GLint Result = GL_FALSE;
+		int InfoLogLength;
+
+
+		// Compile Vertex Shader
+		printf("Compiling shader : %s\n", vertex_file_path);
+		char const * VertexSourcePointer = VertexShaderCode.c_str();
+		glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+		glCompileShader(VertexShaderID);
+
+		// Check Vertex Shader
+		glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+		glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0) {
+			std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+			glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+			printf("%s\n", &VertexShaderErrorMessage[0]);
+		}
+
+
+
+		// Compile Fragment Shader
+		printf("Compiling shader : %s\n", fragment_file_path);
+		char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+		glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+		glCompileShader(FragmentShaderID);
+
+		// Check Fragment Shader
+		glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+		glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0) {
+			std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+			glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+			printf("%s\n", &FragmentShaderErrorMessage[0]);
+		}
+
+
+
+		// Link the program
+		printf("Linking program\n");
+		GLuint ProgramID = glCreateProgram();
+		glAttachShader(ProgramID, VertexShaderID);
+		glAttachShader(ProgramID, FragmentShaderID);
+		glLinkProgram(ProgramID);
+
+		// Check the program
+		glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+		glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0) {
+			std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+			glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+			printf("%s\n", &ProgramErrorMessage[0]);
+		}
+
+
+		glDetachShader(ProgramID, VertexShaderID);
+		glDetachShader(ProgramID, FragmentShaderID);
+
+		glDeleteShader(VertexShaderID);
+		glDeleteShader(FragmentShaderID);
+
+		return ProgramID;
+	}
 
 	App::App(const std::string& title, const int width, const int height)
 		: m_title(title)
@@ -25,6 +131,8 @@ namespace Engine
 
 	App::~App()
 	{
+		glDeleteBuffers(1, &VertexBufferObject);
+		glDeleteVertexArrays(1, &VertexArrayObject);
 		CleanupSDL();
 	}
 
@@ -37,6 +145,57 @@ namespace Engine
 		}
 
 		m_state = GameState::RUNNING;
+
+		//MOVE THIS
+
+		ProgramID = LoadShaders("vertex.glsl", "frag.glsl");
+
+		// set up vertex data (and buffer(s)) and configure vertex attributes
+		// ------------------------------------------------------------------
+		//float vertices[] = {
+		//	-0.5f, -0.5f, 0.0f, // left  
+		//	0.5f, -0.5f, 0.0f, // right 
+		//	0.0f,  0.5f, 0.0f  // top   
+		//};
+		float vertices[] = {
+			// first triangle
+			0.5f,  0.5f, 0.0f,  // top right
+			0.5f, -0.5f, 0.0f,  // bottom right
+			-0.5f,  0.5f, 0.0f, // top left 
+			// second triangle
+			0.5f, -0.5f, 0.0f,  // bottom right
+			-0.5f, -0.5f, 0.0f, // bottom left
+			-0.5f,  0.5f, 0.0f  // top left
+		};
+
+		glGenVertexArrays(1, &VertexArrayObject);
+		glGenBuffers(1, &VertexBufferObject);
+
+		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		glBindVertexArray(VertexArrayObject);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			3 * sizeof(float),  // stride
+			(void*)0            // array buffer offset
+		);
+		glEnableVertexAttribArray(0);
+
+		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+		glBindVertexArray(0);
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 		SDL_Event event;
 		while (m_state == GameState::RUNNING)
@@ -67,7 +226,7 @@ namespace Engine
 
 		// Setup the viewport
 		//
-		SetupViewport();
+		//SetupViewport();
 
 		// Change game state
 		//
@@ -127,12 +286,10 @@ namespace Engine
 		glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_LINE_LOOP);
-		glVertex2f(50.0, 50.0);
-		glVertex2f(50.0, -50.0);
-		glVertex2f(-50.0, -50.0);
-		glVertex2f(-50.0, 50.0);
-		glEnd();
+		//Use the program
+		glUseProgram(ProgramID);
+		glBindVertexArray(VertexArrayObject);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		SDL_GL_SwapWindow(m_mainWindow);
 	}
@@ -146,6 +303,11 @@ namespace Engine
 			std::cerr << "Failed to init SDL" << std::endl;
 			return false;
 		}
+
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -206,6 +368,7 @@ namespace Engine
 
 	bool App::GlewInit()
 	{
+		glewExperimental = GL_TRUE; //Allows features to work 
 		GLenum err = glewInit();
 		if (GLEW_OK != err)
 		{
