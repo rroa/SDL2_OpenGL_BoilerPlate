50ad1ad288706421059c36b6ff7e45ca3dc4597c
Raul Roa <raul.roa@gmail.com>
2018-03-21 07:48:29 -0400

    Updates default rendering code to use the programmable pipeline rather than using the fixed function pipeline.

--- BoilerPlate/App.cpp
+++ BoilerPlate/App.cpp
@@ -6,10 +6,159 @@
 #include <GL/glew.h>
 #include <SDL2/SDL_opengl.h>
 
+//
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+
+//
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 namespace Engine
 {
 	const float DESIRED_FRAME_RATE = 60.0f;
 	const float DESIRED_FRAME_TIME = 1.0f / DESIRED_FRAME_RATE;
+
+	//
+	GLuint VertexArrayObject;  // VAO
+	GLuint VertexBufferObject; // VBO
+	GLuint ElementBufferObject;// EBO
+	GLuint ProgramID;
+	GLuint Texture1;
+	GLuint Texture2;
+
+	GLuint LoadTexture(const char * texture_path)
+	{
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		// set the texture wrapping/filtering options (on the currently bound texture object)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+		int width, height, nrChannels;
+		stbi_set_flip_vertically_on_load(true);
+
+		// Load the texture
+		unsigned char *data = stbi_load(texture_path, &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+			return -1;
+		}
+		stbi_image_free(data);
+
+		return texture;
+	}
+
+	GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path) {
+
+		// Create the shaders
+		GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+		GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+		// Read the Vertex Shader code from the file
+		std::string VertexShaderCode;
+		std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+		if (VertexShaderStream.is_open()) {
+			std::stringstream sstr;
+			sstr << VertexShaderStream.rdbuf();
+			VertexShaderCode = sstr.str();
+			VertexShaderStream.close();
+		}
+		else {
+			printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
+			getchar();
+			return 0;
+		}
+
+		// Read the Fragment Shader code from the file
+		std::string FragmentShaderCode;
+		std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+		if (FragmentShaderStream.is_open()) {
+			std::stringstream sstr;
+			sstr << FragmentShaderStream.rdbuf();
+			FragmentShaderCode = sstr.str();
+			FragmentShaderStream.close();
+		}
+
+		GLint Result = GL_FALSE;
+		int InfoLogLength;
+
+
+		// Compile Vertex Shader
+		printf("Compiling shader : %s\n", vertex_file_path);
+		char const * VertexSourcePointer = VertexShaderCode.c_str();
+		glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+		glCompileShader(VertexShaderID);
+
+		// Check Vertex Shader
+		glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+		glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0) {
+			std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+			glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+			printf("%s\n", &VertexShaderErrorMessage[0]);
+		}
+
+
+
+		// Compile Fragment Shader
+		printf("Compiling shader : %s\n", fragment_file_path);
+		char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+		glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+		glCompileShader(FragmentShaderID);
+
+		// Check Fragment Shader
+		glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+		glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0) {
+			std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+			glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+			printf("%s\n", &FragmentShaderErrorMessage[0]);
+		}
+
+		// Link the program
+		printf("Linking program\n");
+		GLuint ProgramID = glCreateProgram();
+		glAttachShader(ProgramID, VertexShaderID);
+		glAttachShader(ProgramID, FragmentShaderID);
+		glLinkProgram(ProgramID);
+
+		// Check the program
+		glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+		glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0) {
+			std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+			glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+			printf("%s\n", &ProgramErrorMessage[0]);
+		}
+
+
+		glDetachShader(ProgramID, VertexShaderID);
+		glDetachShader(ProgramID, FragmentShaderID);
+
+		glDeleteShader(VertexShaderID);
+		glDeleteShader(FragmentShaderID);
+
+		return ProgramID;
+	}
 
 	App::App(const std::string& title, const int width, const int height)
 		: m_title(title)
@@ -25,6 +174,11 @@ namespace Engine
 
 	App::~App()
 	{
+		// Cleanup
+		glDeleteBuffers(1, &VertexBufferObject);
+		glDeleteVertexArrays(1, &VertexArrayObject);
+		glDeleteProgram(ProgramID);
+
 		CleanupSDL();
 	}
 
@@ -37,6 +191,110 @@ namespace Engine
 		}
 
 		m_state = GameState::RUNNING;
+
+		// TODO: RR: Move this out!
+
+		// Create and compile our GLSL program from the shaders
+		ProgramID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+		Texture1 = LoadTexture("test.png");
+		Texture2 = LoadTexture("face.png");
+
+		// set up vertex data (and buffer(s)) and configure vertex attributes
+		// ------------------------------------------------------------------
+		//float vertices[] = {
+		//	0.5f,  0.5f, 0.0f,  // top right
+		//	0.5f, -0.5f, 0.0f,  // bottom right
+		//	-0.5f, -0.5f, 0.0f,  // bottom left
+		//	-0.5f,  0.5f, 0.0f   // top left 
+		//};
+
+		//float vertices[] = {
+		//	// positions          // colors         
+		//	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // top right
+		//	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // bottom right
+		//	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // bottom left
+		//	-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f, // top left 
+		//};
+
+		float vertices[] = {
+			// positions          // colors           // texture coords
+			0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+			0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+			-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+			-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+		};
+
+		//float vertices[] = {
+		//	// positions          // colors           // texture coords
+		//	1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		//	1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		//	-1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		//	-1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+		//};
+
+		unsigned int indices[] = {  // note that we start from 0!
+			0, 1, 3,  // first Triangle
+			1, 2, 3   // second Triangle
+		};
+
+		glGenVertexArrays(1, &VertexArrayObject);
+		glGenBuffers(1, &VertexBufferObject);
+		glGenBuffers(1, &ElementBufferObject);
+
+		glBindVertexArray(VertexArrayObject);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ElementBufferObject);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		// vertex position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		// color attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		// texture coord attribute
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glBindVertexArray(0);
+
+		glUseProgram(ProgramID);
+
+		// Remember this needs to be set after the program is activated
+		glUniform1i(glGetUniformLocation(ProgramID, "texture1"), 0);
+		glUniform1i(glGetUniformLocation(ProgramID, "texture2"), 1);
+
+		float resolution[] = { static_cast<float>(m_width), static_cast<float>(m_height) };
+		glUniform2fv(glGetUniformLocation(ProgramID, "resolution"), 1, resolution);
+
+
+		// create transformations
+		glm::mat4 model;
+		glm::mat4 view;
+		glm::mat4 projection;
+
+		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(glm::radians(45.0f), static_cast<float>(m_width) / static_cast<float>(m_height), 0.1f, 100.0f);
+		
+		// retrieve the matrix uniform locations
+		GLuint modelLoc = glGetUniformLocation(ProgramID, "model");
+		GLuint viewLoc = glGetUniformLocation(ProgramID, "view");
+		GLuint projectionLoc = glGetUniformLocation(ProgramID, "projection");
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 		SDL_Event event;
 		while (m_state == GameState::RUNNING)
@@ -67,7 +325,7 @@ namespace Engine
 
 		// Setup the viewport
 		//
-		SetupViewport();
+		//SetupViewport();
 
 		// Change game state
 		//
@@ -105,6 +363,7 @@ namespace Engine
 
 		// Update code goes here
 		//
+		glUniform1f(glGetUniformLocation(ProgramID, "time"), static_cast<float>(startTime));
 
 		double endTime = m_timer->GetElapsedTimeInSeconds();
 		double nextTimeFrame = startTime + DESIRED_FRAME_TIME;
@@ -127,13 +386,16 @@ namespace Engine
 		glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_LINE_LOOP);
-		glVertex2f(50.0, 50.0);
-		glVertex2f(50.0, -50.0);
-		glVertex2f(-50.0, -50.0);
-		glVertex2f(-50.0, 50.0);
-		glEnd();
+		// bind Texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture1);
 
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, Texture2);
+
+		glBindVertexArray(VertexArrayObject);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 		SDL_GL_SwapWindow(m_mainWindow);
 	}
 
@@ -146,6 +408,13 @@ namespace Engine
 			std::cerr << "Failed to init SDL" << std::endl;
 			return false;
 		}
+
+		// This means that the old, deprecated code are disabled, only the newer versions can be used.
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		// Request opengl 3.2 context.
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -187,25 +456,25 @@ namespace Engine
 		float halfHeight = m_height * 0.5f;
 
 		// Set viewport to match window
-		//
 		glViewport(0, 0, m_width, m_height);
 
 		// Set Mode to GL_PROJECTION
-		//
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 
 		// Set projection MATRIX to ORTHO
-		//
 		glOrtho(-halfWidth, halfWidth, -halfHeight, halfHeight, -1, 1);
 
 		// Setting Mode to GL_MODELVIEW
-		//
 		glMatrixMode(GL_MODELVIEW);
 	}
 
 	bool App::GlewInit()
 	{
+		// Let's tell GLEW we want everything available for the graphics pipeline
+		// Specially when using OpenGL's core profile
+		glewExperimental = GL_TRUE;
+
 		GLenum err = glewInit();
 		if (GLEW_OK != err)
 		{
